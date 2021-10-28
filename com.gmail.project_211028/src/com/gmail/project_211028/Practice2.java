9f0e450150ba3ea632c25b0b7666bc19d1066d2b
tj <tj@DESKTOP-37MC1TR>
2021-10-28 18:19:46 +0900

    20211028

---
+++ com.gmail.project_211028/src/com/gmail/project_211028/Practice2.java
@@ -0,0 +1,21 @@
+package com.gmail.project_211028;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		//                     첫번째별  두번째별
+		//    *     5     i=0  5-i     5+i
+        //   * *    4 6   i=1  
+		//  *   *   3 7   i=2  
+		// *     *  2 8   i=3  
+		//********* 1 9   i=4  
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<9; j++) {
+				System.out.print(" ");
+			}
+
+			System.out.println("");
+		}
+	}
+
+}
